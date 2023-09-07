8eedbbf6417a8dcde60bb16970a22424964d0aa4
mingseobi <22200078@handong.ac.kr>
2023-09-07 23:43:52 +0900

    final Updated

--- src/main/java/org/example/WordManager.java
+++ src/main/java/org/example/WordManager.java
@@ -30,7 +30,10 @@ public class WordManager {
 
         while(true) {
             int menu = selectMenu();
-            if(menu == 0) break;
+            if(menu == 0) {
+                System.out.print("\n프로그램 종료! 다음에 만나요~");
+                break;
+            }
             if(menu == 4) {
                 wordCRUD.addWord();
             }
