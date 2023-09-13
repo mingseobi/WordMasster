da5735f6ae6656731d47b2923b25246d802a6f62
mingseobi <22200078@handong.ac.kr>
2023-09-14 00:41:05 +0900

    Updated 구현 완료

--- src/main/java/org/example/WordManager.java
+++ src/main/java/org/example/WordManager.java
@@ -41,6 +41,15 @@ public class WordManager {
             else if(menu == 1) {
                 wordCRUD.listAll();
             }
+
+            else if(menu == 5) {
+                wordCRUD.updateItem();
+            }
+
+            else if(menu == 6) {
+                wordCRUD.listAll();
+            }
+
         }
     }
 }
